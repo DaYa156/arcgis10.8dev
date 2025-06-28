30d81c7d639d8c16f285386137b0ec819084794f
DaYa156 <1561674030@qq.com>
2025-06-28 15:25:00 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿using ESRI.ArcGIS;
+using ESRI.ArcGIS.Controls;     // ControlsOpenDocCommand、ControlsMapZoomInTool、esriCommandStyles 都在这里
+using ESRI.ArcGIS.esriSystem;   // 如果你要用 IAoInitialize、ProductCode 枚举
+namespace 秦淮河流域概览
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
+            this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(247, 57);
+            this.axMapControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(541, 381);
+            this.axMapControl1.TabIndex = 0;
+            // 
+            // axTOCControl1
+            // 
+            this.axTOCControl1.Location = new System.Drawing.Point(12, 57);
+            this.axTOCControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.axTOCControl1.Name = "axTOCControl1";
+            this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
+            this.axTOCControl1.Size = new System.Drawing.Size(229, 381);
+            this.axTOCControl1.TabIndex = 1;
+            // 
+            // axToolbarControl1
+            // 
+            this.axToolbarControl1.Location = new System.Drawing.Point(12, 12);
+            this.axToolbarControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.axToolbarControl1.Name = "axToolbarControl1";
+            this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
+            this.axToolbarControl1.Size = new System.Drawing.Size(776, 28);
+            this.axToolbarControl1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 453);
+            this.Controls.Add(this.axToolbarControl1);
+            this.Controls.Add(this.axTOCControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ESRI.ArcGIS;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.SystemUI;
+
+namespace 秦淮河流域概览
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Load += Form1_Load;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 绑定运行时与许可
+            //RuntimeManager.Bind(ProductCode.EngineOrDesktop);
+            //ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.EngineOrDesktop);
+
+            // 配对
+            //axToolbarControl1.SetBuddyControl(axMapControl1);
+            //axTOCControl1.SetBuddyControl(axMapControl1);
+
+            // 打开文档命令
+            var openCmd = new ControlsOpenDocCommand();
+            axToolbarControl1.AddItem(openCmd, -1, -1, false, 0, esriCommandStyles.esriCommandStyleIconOnly);
+            openCmd.OnCreate(axMapControl1.Object);
+
+            // 缩放、漫游工具
+            var zoomIn = new ControlsMapZoomInTool();
+            axToolbarControl1.AddItem(zoomIn, -1, -1, false, 0, esriCommandStyles.esriCommandStyleIconOnly);
+            zoomIn.OnCreate(axMapControl1.Object);
+
+            var panTool = new ControlsMapPanTool();
+            axToolbarControl1.AddItem(panTool, -1, -1, false, 0, esriCommandStyles.esriCommandStyleIconOnly);
+            panTool.OnCreate(axMapControl1.Object);
+        }
+    }
+}
